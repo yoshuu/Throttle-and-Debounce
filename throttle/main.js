3263a4d99d5b201503dd5170b8b9bd954910d74e
yoshuu <appcs342@gmail.com>
2023-04-23 11:02:20 +0800

    update throttle fn + html

--- throttle/main.js
+++ throttle/main.js
@@ -1,1 +1,44 @@
-const throttle = (callback, time = 1000) => {};
+console.log("test0");
+const throttle = (callback, time = 1000) => {
+  //閉包存起來
+  let timer;
+
+  return () => {
+    if (timer) {
+      return;
+    }
+
+    timer = setTimeout(() => {
+      callback();
+
+      timer = null;
+    }, time);
+  };
+};
+
+const scroll = () => {
+  let clientHeight = document.documentElement.clientHeight;
+  let scrollTop = document.documentElement.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight;
+
+  console.log("clientHeight", clientHeight);
+  console.log("scrollTop", scrollTop);
+  console.log("scrollHeight", scrollHeight);
+
+  if ((scrollTop + clientHeight) / scrollHeight >= 0.9) {
+    for (let i = 0; i <= 5; i++) {
+      let p = document.createElement("p");
+      p.textContent = "新增新增新增";
+      main.appendChild(p);
+    }
+  }
+};
+
+console.log("test");
+
+const main = document.querySelector(".main");
+
+// let c = document.documentElement.clientHeight;
+// console.log(c);
+
+document.addEventListener("scroll", throttle(scroll));
